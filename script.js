27f5c372fae3c8429130ff2581e77b24eacc0429
Nikita Tishunets <tischunez@gmail.com>
2024-01-25 22:13:44 +0300

    Update

---
+++ script.js
@@ -0,0 +1,12 @@
+$.fn.animate_Text = function() {
+    var string = this.text();
+    return this.each(function(){
+     var $this = $(this);
+     $this.html(string.replace(/./g, '<span class="new">$&</span>'));
+     $this.find('span.new').each(function(i, el){
+      setTimeout(function(){ $(el).addClass('div_opacity'); }, 20 * i);
+     });
+    });
+   };
+   $('#example').show();
+   $('#example').animate_Text();
